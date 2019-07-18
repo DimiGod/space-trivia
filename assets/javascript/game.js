8d74c07b64d04fdeb57975ed1fbeba0d29e7c628
Dimitrios Tsioutsiopoulos <dimitrios.g0503@gmail.com>
2019-07-18 18:55:31 -0400

    added a few q/a to the array on the js document.

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+//...array to hold each question object
+var questionArr = [
+    {   question:"How many space shuttles were commissioned by NASA ?",
+        choices:['a: 129','b: 37','c: 6','d: 13'],
+        ans:'c'
+    },
+    {   question:"How many rovers were commissioned to Mars ?",
+        choices:['a: 6','b: 8','c: 2','d: 14'],
+        ans:'a'
+    },
+    {   question:"How many miles is the moon from the earth ?",
+        choices:['a: 83,740','b: 238,900','c: 375,600','d: 1,417,530'],
+        ans:'b'
+    },
+    {   question:"How many squares are there on a chess board ?",
+        choices:['a: 40','b: 50','c: 60','d: 70'],
+        ans:'a'
+    },
+    {   question:"What is the largest country by area that only has one time zone ?",
+        choices:['a: Mexico','b: United States','c: Rusia','d: China'],
+        ans:'d'
+    }]
